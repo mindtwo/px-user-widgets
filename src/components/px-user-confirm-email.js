cad1d7840f3301da8d1340f303c9042d8ee7bef1
Andreas Schneider <schneider@mindtwo.de>
2024-02-14 18:27:18 +0100

    add confirm email form

---
+++ src/components/px-user-confirm-email.js
@@ -0,0 +1,43 @@
+import {UserComponent} from './user-component';
+
+export class PxUserConfirmEmail extends UserComponent {
+    /**
+     * Mount iframe
+     */
+    mountIFrame() {
+        const fallbackTargetUrl = `${this.appUrl}/api/v1/confirm-new-email`;
+
+        this.module.showConfirmNewEmailForm({
+            token: this.token,
+            containerElement: this.containerId,
+            fallbackTargetUrl,
+            fallbackButtonText: 'Confirm New Email',
+            cssUrl: this.cssUrl,
+            icons: {
+                togglePassword: {
+                    passwordOne: true,
+                    passwordTwo: true
+                }
+            },
+            onSuccess: (response) => {
+                if (! response.success) {
+                    return this.handleError(response);
+                }
+
+                window.dispatchEvent(new CustomEvent('px-user-email-confirmed', {
+                    detail: data,
+                }));
+            },
+            onError: (error) => this.handleError(error),
+        });
+    }
+
+    /**
+     * Get container fallback id
+     *
+     * @returns {String}
+     */
+     getContainerId() {
+        return 'px-user-confirm-email';
+    }
+}
